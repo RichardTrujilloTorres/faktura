6cfeed88432f6ca74608f6860550847c2f5f5321
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-06-09 15:16:25 +0200

    Add main layout and its partials

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/apple-icon.png') }}">
+  <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+  <title>
+      {{ config('app.name') }}
+  </title>
+
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+
+  <!-- CSS Files -->
+  <link href="{{ asset('css/material-dashboard.css?v=2.1.1') }}" rel="stylesheet" />
+
+</head>
+
+<body class="">
+  <div class="wrapper ">
+    <div class="sidebar" data-color="purple" data-background-color="white" data-image="{{ asset('img/sidebar-1.jpg') }}">
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+        Tip 2: you can also add an image using data-image tag
+    -->
+      <div class="logo">
+        <a href="{{ url('/') }}" class="simple-text logo-normal">
+            {{ config('app.name') }}
+        </a>
+      </div>
+      <div class="sidebar-wrapper">
+        <ul class="nav">
+
+        @if (Route::is('dashboard'))
+          <li class="nav-item active  ">
+        @else
+            <li class="nav-item ">
+        @endif
+            <a class="nav-link" href="{{ route('dashboard') }}">
+              <i class="material-icons">dashboard</i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+
+            @if (Route::is('invoices.*'))
+              <li class="nav-item active  ">
+            @else
+              <li class="nav-item ">
+            @endif
+            <a class="nav-link" href="{{ route('invoices.index') }}">
+              <i class="material-icons">person</i>
+              <p>Invoices</p>
+            </a>
+          </li>
+
+          {{--
+          <li class="nav-item ">
+            <a class="nav-link" href="./tables.html">
+              <i class="material-icons">content_paste</i>
+              <p>Table List</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="./typography.html">
+              <i class="material-icons">library_books</i>
+              <p>Typography</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="./icons.html">
+              <i class="material-icons">bubble_chart</i>
+              <p>Icons</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="./map.html">
+              <i class="material-icons">location_ons</i>
+              <p>Maps</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="./notifications.html">
+              <i class="material-icons">notifications</i>
+              <p>Notifications</p>
+            </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="./rtl.html">
+              <i class="material-icons">language</i>
+              <p>RTL Support</p>
+            </a>
+          </li>
+          <li class="nav-item active-pro ">
+            <a class="nav-link" href="./upgrade.html">
+              <i class="material-icons">unarchive</i>
+              <p>Upgrade to PRO</p>
+            </a>
+          </li>
+          --}}
+        </ul>
+      </div>
+    </div>
+    <div class="main-panel">
+
+      @include('partials._navbar')
+
+      <div class="content" id="app">
+        <div class="container-fluid">
+            @yield('content')
+        </div>
+      </div>
+
+      @include('partials._footer')
+
+    </div>
+  </div>
+
+
+  <!--   Core JS Files   -->
+  <script src="{{ asset('js/core/jquery.min.js') }}"></script>
+  <script src="{{ asset('js/core/popper.min.js') }}"></script>
+  <script src="{{ asset('js/core/bootstrap-material-design.min.js') }}"></script>
+
+
+  <script src="{{ asset('js/plugins/perfect-scrollbar.jquery.min.js') }}"></script>
+  <!-- Plugin for the momentJs  -->
+  <script src="{{ asset('js/plugins/moment.min.js') }}"></script>
+  <!--  Plugin for Sweet Alert -->
+  <script src="{{ asset('js/plugins/sweetalert2.js') }}"></script>
+  <!-- Forms Validations Plugin -->
+  <script src="{{ asset('js/plugins/jquery.validate.min.js') }}"></script>
+  <!-- Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
+  <script src="{{ asset('js/plugins/jquery.bootstrap-wizard.js') }}"></script>
+  <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+  <script src="{{ asset('js/plugins/bootstrap-selectpicker.js') }}"></script>
+  <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+  <script src="{{ asset('js/plugins/bootstrap-datetimepicker.min.js') }}"></script>
+  <!--  DataTables.net Plugin, full documentation here: https://datatables.net/  -->
+  <script src="{{ asset('js/plugins/jquery.dataTables.min.js') }}"></script>
+  <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+  <script src="{{ asset('js/plugins/bootstrap-tagsinput.js') }}"></script>
+  <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+  <script src="{{ asset('js/plugins/jasny-bootstrap.min.js') }}"></script>
+  <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
+  <script src="{{ asset('js/plugins/fullcalendar.min.js') }}"></script>
+  <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
+  <script src="{{ asset('js/plugins/jquery-jvectormap.js') }}"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="{{ asset('js/plugins/nouislider.min.js') }}"></script>
+  <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
+  <!-- Library for adding dinamically elements -->
+  <script src="{{ asset('js/plugins/arrive.min.js') }}"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Chartist JS -->
+  <script src="{{ asset('js/plugins/chartist.min.js') }}"></script>
+  <!--  Notifications Plugin    -->
+  <script src="{{ asset('js/plugins/bootstrap-notify.js') }}"></script>
+
+  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="{{ asset('js/material-dashboard.js?v=2.1.1') }}" type="text/javascript"></script>
+
+</body>
+
+</html>
---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,21 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <nav class="float-left">
+            <ul>
+                <li>
+                    <a href="">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Documentation
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright float-right">
+            &copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-coffee"></i> by <a href="">Richard Trujillo</a>
+        </div>
+    </div>
+</footer>
