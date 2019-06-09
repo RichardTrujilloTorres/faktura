1e99e18b12fe2f41f84ba6cb53ccb7e613f59b75
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-06-09 16:21:20 +0200

    Add invoice model and its migration

---
+++ database/migrations/2019_06_09_141040_create_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('original_name');
+            $table->string('filename')->unique();
+            $table->string('number')->nullable();
+            $table->string('file_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
