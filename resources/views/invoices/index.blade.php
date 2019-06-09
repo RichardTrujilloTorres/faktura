f009a337e35f149e67d8d2fab7f631a9b1ff2298
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-06-09 19:10:52 +0200

    Add invoice creation link on listing view

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -2,5 +2,13 @@
 
 
 @section('content')
-    // invoices
+    <div class="row">
+        <div class="col-md-6">
+            <a class="btn btn-primary btn-fab btn-fab-mini btn-round" href="{{ route('invoices.create') }}">
+                <i class="material-icons">add</i>
+            </a>
+        </div>
+    </div>
+
+    // invoices listing
 @endsection
