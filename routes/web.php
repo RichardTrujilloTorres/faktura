b1c52e00cdb62800f8a3414ab5551f8a1f8a8356
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-06-09 15:15:41 +0200

    Register dashboard route

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
 
 Auth::routes();
 
