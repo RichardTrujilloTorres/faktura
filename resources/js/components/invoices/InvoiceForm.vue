89df8898ae184681b1dd2e889621216a4a6481e6
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-06-09 16:22:12 +0200

    Add invoice creation form component

---
+++ resources/js/components/invoices/InvoiceForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="invoice-form" @submit.prevent="submit">
+        <form enctype="multipart/form-data">
+            <file-upload></file-upload>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            form: {
+                file: null
+            }
+        }),
+        mounted() {
+        },
+
+        methods: {
+        }
+    }
+</script>
