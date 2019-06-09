d15c2d70befa5f6d5e61cb59a14534d246cde757
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-06-09 19:05:21 +0200

    Add url section to faktura config file

--- config/faktura.php
+++ config/faktura.php
@@ -12,4 +12,16 @@ return [
     'storage' => [
         'directory' => env('STORAGE_DIR', 'invoices'),
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Url
+    |--------------------------------------------------------------------------
+    |
+    | Project related urls.
+    */
+    'url' => [
+        'github' => env('FAKTURA_GITHUB_URL', 'https://github.com/RichardTrujilloTorres/faktura'),
+        'docs' => env('FAKTURA_DOCS_URL', ''),
+    ],
 ];
